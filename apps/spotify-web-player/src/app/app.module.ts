8aebc5d4d24d7bfb90230d6f7786ad0eb61e7371
Diego Pascual <diegopascual.f@gmail.com>
2021-09-10 19:39:22 +0200

    feat(#1): fetch access token during app bootstrap

--- apps/spotify-web-player/src/app/app.module.ts
+++ apps/spotify-web-player/src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AuthService } from '@spotify-web-player/auth';
 import { AppComponent } from './app.component';
+import { appInitProviderFactory } from './core/initializers/appInitProvider.factory';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitProviderFactory,
+      deps: [AuthService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
