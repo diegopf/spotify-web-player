62659b7416f74a5bc5c61f79db9b92da9fbe3785
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 20:38:46 +0200

    feat(#7): add chip component

---
+++ libs/shared/src/lib/chip/chip.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ChipComponent } from './components/chip/chip.component';
+
+@NgModule({
+  imports: [],
+  exports: [ChipComponent],
+  declarations: [ChipComponent],
+  providers: [],
+})
+export class ChipModule {}
