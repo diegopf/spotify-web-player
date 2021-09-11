7fc0e6186a40bb060bc817c0aaf12760158b99d3
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 23:18:58 +0200

    feat(#9): add album route

---
+++ libs/feature/album/src/lib/components/album-view/album-view.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyApiService } from '@spotify-web-player/api';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'spotify-web-player-album-view',
+  templateUrl: './album-view.component.html',
+  styleUrls: ['./album-view.component.scss'],
+})
+export class AlbumViewComponent implements OnInit {
+  album: SpotifyApi.SingleAlbumResponse | undefined;
+  private subscription = new Subscription();
+  constructor(
+    private spotifyApiService: SpotifyApiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this.route.params.subscribe(({ id }) => {
+        this.spotifyApiService
+          .getAlbum(id)
+          .subscribe((album) => (this.album = album));
+      })
+    );
+  }
+
+  get albumName() {
+    return this.album?.name ?? '';
+  }
+
+  get albumCover() {
+    return this.album?.images[0].url;
+  }
+
+  get tracks() {
+    return this.album?.tracks.items ?? [];
+  }
+}
--- libs/feature/album/src/lib/feature-album.module.ts
+++ libs/feature/album/src/lib/feature-album.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ImageModule } from '@spotify-web-player/shared';
+import { AlbumViewComponent } from './components/album-view/album-view.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AlbumViewComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes), ImageModule],
+  declarations: [AlbumViewComponent],
 })
 export class FeatureAlbumModule {}
