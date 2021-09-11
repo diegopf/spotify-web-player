caa792f4070b47f65243b79d5fc272292cd0b912
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 20:00:20 +0200

    fix: unsubscribe onDestroy search view

--- libs/feature/search/src/lib/components/search-view/search-view.component.ts
+++ libs/feature/search/src/lib/components/search-view/search-view.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SpotifyApiService } from '@spotify-web-player/api';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'spotify-web-player-search-view',
   templateUrl: './search-view.component.html',
   styleUrls: ['./search-view.component.scss'],
 })
-export class SearchViewComponent implements OnInit {
+export class SearchViewComponent implements OnInit, OnDestroy {
   artists: SpotifyApi.PagingObject<SpotifyApi.ArtistObjectFull> | undefined;
   albums: SpotifyApi.PagingObject<SpotifyApi.AlbumObjectSimplified> | undefined;
   tracks: SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull> | undefined;
   emptyResults = false;
+  private subscription = new Subscription();
   constructor(
     private spotifyApiService: SpotifyApiService,
     private router: Router,
@@ -19,19 +21,25 @@ export class SearchViewComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(({ searchKeywords }) => {
-      if (searchKeywords) {
-        this.spotifyApiService.search(searchKeywords).subscribe((data) => {
-          this.artists = data.artists;
-          this.albums = data.albums;
-          this.tracks = data.tracks;
-          this.emptyResults =
-            data.artists?.items.length === 0 &&
-            data.albums?.items.length === 0 &&
-            data.tracks?.items.length === 0;
-        });
-      }
-    });
+    this.subscription.add(
+      this.route.queryParams.subscribe(({ searchKeywords }) => {
+        if (searchKeywords) {
+          this.spotifyApiService.search(searchKeywords).subscribe((data) => {
+            this.artists = data.artists;
+            this.albums = data.albums;
+            this.tracks = data.tracks;
+            this.emptyResults =
+              data.artists?.items.length === 0 &&
+              data.albums?.items.length === 0 &&
+              data.tracks?.items.length === 0;
+          });
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   search(searchKeywords: string) {
