f5c98bb67909ec79264f66f140f48f8a04927ad1
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 18:53:47 +0200

    test(#5): add test for thumbnail component

--- libs/shared/src/lib/image/components/thumbnail/thumbnail.component.spec.ts
+++ libs/shared/src/lib/image/components/thumbnail/thumbnail.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { ThumbnailComponent } from './thumbnail.component';
 
 describe('ThumbnailComponent', () => {
@@ -8,18 +8,28 @@ describe('ThumbnailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ThumbnailComponent ]
-    })
-    .compileComponents();
+      declarations: [ThumbnailComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ThumbnailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should reflect the input given', () => {
+    component.ariaLabel = 'Some aria label';
+    component.image = 'img';
+    component.width = 300;
+    component.height = 100;
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('img'));
+    expect(element.styles['width']).toBe('300px');
+    expect(element.styles['height']).toBe('100px');
+    expect(element.attributes['src']).toBe('img');
   });
 });
