14032623707da6cefece0ebe4a0fcbcd24506f65
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 21:09:06 +0200

    feat(#7): add artist related api methods

--- libs/feature/api/src/lib/services/spotify-api.service.ts
+++ libs/feature/api/src/lib/services/spotify-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 @Injectable({
@@ -9,7 +10,17 @@ export class SpotifyApiService {
   private baseApiUrl = 'https://api.spotify.com/v1';
   constructor(private http: HttpClient) {}
 
-  search(searchText: string, type: string = 'artist,album,track') {
+  /**
+   *
+   * @param searchText {string} the text to search for
+   * @param type {string} scope for the search
+   * @returns {Observable<SpotifyApi.SearchResponse>} r
+   *  retuns an observable which will resolve with the results that matches the query
+   */
+  search(
+    searchText: string,
+    type: string = 'artist,album,track'
+  ): Observable<SpotifyApi.SearchResponse> {
     return this.http
       .get<SpotifyApi.SearchResponse>(
         `${this.baseApiUrl}/search?q=${searchText}&type=${type}`
@@ -17,9 +28,61 @@ export class SpotifyApiService {
       .pipe(take(1));
   }
 
-  getArtist(id: string) {
+  /**
+   *
+   * @param id {string} artist id to search for
+   * @returns {Observable<SpotifyApi.SingleArtistResponse>}
+   * returns an observable which will resolve the artist related info
+   */
+  getArtist(id: string): Observable<SpotifyApi.SingleArtistResponse> {
     return this.http
       .get<SpotifyApi.SingleArtistResponse>(`${this.baseApiUrl}/artists/${id}`)
+      .pipe(take(1));
+  }
+
+  /**
+   *
+   * @param id {string} artist id to search for
+   * @returns {Observable<SpotifyApi.ArtistsAlbumsResponse>}
+   * returns an observable which will resolve the artist's albums
+   */
+  getArtistAlbums(id: string): Observable<SpotifyApi.ArtistsAlbumsResponse> {
+    return this.http
+      .get<SpotifyApi.ArtistsAlbumsResponse>(
+        `${this.baseApiUrl}/artists/${id}/albums`
+      )
+      .pipe(take(1));
+  }
+
+  /**
+   *
+   * @param id {string} artist id to search for
+   * @returns {Observable<SpotifyApi.ArtistsRelatedArtistsResponse>}
+   * returns an observable which will resolve the related artist
+   */
+  getArtistRelatedArtists(
+    id: string
+  ): Observable<SpotifyApi.ArtistsRelatedArtistsResponse> {
+    return this.http
+      .get<SpotifyApi.ArtistsRelatedArtistsResponse>(
+        `${this.baseApiUrl}/artists/${id}/related-artists`
+      )
+      .pipe(take(1));
+  }
+
+  /**
+   *
+   * @param id {string} artist id to search for
+   * @returns {Observable<SpotifyApi.ArtistsTopTracksResponse>}
+   * returns an observable which will resolve the artist's top tracks
+   */
+  getArtistTopTracks(
+    id: string
+  ): Observable<SpotifyApi.ArtistsTopTracksResponse> {
+    return this.http
+      .get<SpotifyApi.ArtistsTopTracksResponse>(
+        `${this.baseApiUrl}/artists/${id}/top-tracks?market=ES`
+      )
       .pipe(take(1));
   }
 }
