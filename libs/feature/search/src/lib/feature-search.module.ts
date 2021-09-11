8dff83831f9fc0d95e33ece22366323c652cc559
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 18:20:23 +0200

    feat(#5): include shared image module into search

--- libs/feature/search/src/lib/feature-search.module.ts
+++ libs/feature/search/src/lib/feature-search.module.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InputModule, ListModule } from '@spotify-web-player/shared';
+import {
+  ImageModule,
+  InputModule,
+  ListModule,
+} from '@spotify-web-player/shared';
+import { SearchLayoutComponent } from './components/search-layout/search-layout.component';
 import { SearchViewComponent } from './components/search-view/search-view.component';
 
 const routes: Routes = [
@@ -17,9 +22,10 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     InputModule,
     ListModule,
+    ImageModule,
   ],
   exports: [SearchViewComponent],
-  declarations: [SearchViewComponent],
+  declarations: [SearchViewComponent, SearchLayoutComponent],
   bootstrap: [SearchViewComponent],
 })
 export class FeatureSearchModule {}
