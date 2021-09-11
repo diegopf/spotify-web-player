f3e5568516955d3ad566d2151047d058734f1c3c
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 02:02:10 +0200

    refactor(#5): use take operator for subscribing to search

--- libs/feature/api/src/lib/services/spotify-api.service.ts
+++ libs/feature/api/src/lib/services/spotify-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +10,10 @@ export class SpotifyApiService {
   constructor(private http: HttpClient) {}
 
   search(searchText: string, type: string = 'artist,album,track') {
-    return this.http.get<SpotifyApi.SearchResponse>(
-      `${this.baseApiUrl}/search?q=${searchText}&type=${type}`
-    );
+    return this.http
+      .get<SpotifyApi.SearchResponse>(
+        `${this.baseApiUrl}/search?q=${searchText}&type=${type}`
+      )
+      .pipe(take(1));
   }
 }
