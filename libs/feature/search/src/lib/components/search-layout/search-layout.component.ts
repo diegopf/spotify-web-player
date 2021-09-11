257d0b383af4236acd24ee1821731c5845495e07
Diego Pascual <diegopascual.f@gmail.com>
2021-09-11 18:21:19 +0200

    feat(#5): add basic search layout component

---
+++ libs/feature/search/src/lib/components/search-layout/search-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spotify-web-player-search-layout',
+  templateUrl: './search-layout.component.html',
+  styleUrls: ['./search-layout.component.scss'],
+})
+export class SearchLayoutComponent {}
