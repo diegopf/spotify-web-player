40160af7eb0cb8bf218e80878ca32e383ac18592
Diego Pascual <diegopascual.f@gmail.com>
2021-09-12 18:08:42 +0200

    feat(#12): make use of the theme and theme service

--- apps/spotify-web-player/src/app/app.component.ts
+++ apps/spotify-web-player/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '@spotify-web-player/shared';
 
 @Component({
   selector: 'spotify-web-player-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private themeService: ThemeService) {}
+  switchTheme() {
+    this.themeService.toggleTheme();
+  }
+}
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -4,5 +4,6 @@ export * from './lib/image/image.module';
 export * from './lib/input/input.module';
 export * from './lib/list/list.module';
 export * from './lib/shared.module';
+export * from './lib/theme/services';
 export * from './lib/track/track.module';
 export * from './lib/view/view.module';
