4b37966b583c8dc01895c3c060c706ba927a2469
Diego Pascual <diegopascual.f@gmail.com>
2021-09-12 22:03:37 +0200

    feat(#14): set up service worker

--- apps/spotify-web-player/src/app/app.module.ts
+++ apps/spotify-web-player/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ThemeSwitchModule } from '@spotify-web-player/shared';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { appInitProviderFactory } from './core/initializers/appInitProvider.factory';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +16,12 @@ import { appInitProviderFactory } from './core/initializers/appInitProvider.fact
     HttpClientModule,
     AppRoutingModule,
     ThemeSwitchModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
