412f4ff92f716b86cbcf9792823a88b3772a5095
Diego Pascual <diegopascual.f@gmail.com>
2021-09-12 19:18:57 +0200

    feat(#12): make use of the new switch theme component

--- apps/spotify-web-player/src/app/app.component.ts
+++ apps/spotify-web-player/src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { ThemeService } from '@spotify-web-player/shared';
 
 @Component({
   selector: 'spotify-web-player-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private themeService: ThemeService) {}
-  switchTheme() {
-    this.themeService.toggleTheme();
-  }
-}
+export class AppComponent {}
--- apps/spotify-web-player/src/app/app.module.ts
+++ apps/spotify-web-player/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthService, TokenInterceptor } from '@spotify-web-player/auth';
+import { ThemeSwitchModule } from '@spotify-web-player/shared';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { appInitProviderFactory } from './core/initializers/appInitProvider.factory';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ThemeSwitchModule,
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
