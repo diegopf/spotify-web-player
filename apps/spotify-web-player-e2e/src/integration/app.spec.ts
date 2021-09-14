3034296830e0c790e16e2282b276ae4c3fa6de1b
Diego Pascual <diegopascual.f@gmail.com>
2021-09-14 20:34:14 +0200

    test(#31): adjust e2e tests

--- apps/spotify-web-player-e2e/src/integration/app.spec.ts
+++ apps/spotify-web-player-e2e/src/integration/app.spec.ts
@@ -4,16 +4,14 @@ describe('spotify-web-player', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getGreeting().contains('Search for your favorite music!');
+    getGreeting().contains('Search for');
   });
 
   it('should open an artist view', () => {
     cy.visit('/search?q=band%20of');
     getSearchInput().should('have.value', 'band of');
     cy.get("a[href*='artist/0OdUWJ0sBjDrqHygGUXeCF']").should('exist').click();
-    cy.get('[data-cy=artist-name')
-      .first()
-      .should('have.text', 'Band of Horses');
+    cy.get('[data-cy=artist-name').first().contains('Band of Horses');
   });
 
   it('should open an album view', () => {
@@ -25,10 +23,10 @@ describe('spotify-web-player', () => {
 
   it('Should set the dark theme', () => {
     cy.get('body').should('have.css', 'background-color', 'rgb(100, 179, 244)');
-    getGreeting().should('have.css', 'color', 'rgb(194, 229, 156)');
+    getSearchInput().should('have.css', 'color', 'rgb(194, 229, 156)');
     getThemeSwitch().click();
     cy.get('body').should('have.css', 'background-color', 'rgb(9, 14, 17)');
-    getGreeting().should('have.css', 'color', 'rgb(27, 97, 98)');
+    getSearchInput().should('have.css', 'color', 'rgb(27, 97, 98)');
     getThemeSwitch().click();
     cy.get('body').should('have.css', 'background-color', 'rgb(100, 179, 244)');
   });
--- apps/spotify-web-player-e2e/src/support/app.po.ts
+++ apps/spotify-web-player-e2e/src/support/app.po.ts
@@ -1,5 +1,5 @@
-export const getGreeting = () => cy.get('h1');
+export const getGreeting = () => cy.get('[data-cy=landing-message]');
 
 export const getSearchInput = () => cy.get('[data-cy=search-input]');
 
-export const getThemeSwitch = () => cy.get('[data-cy=theme-switch');
+export const getThemeSwitch = () => cy.get('[data-cy=theme-switch]');
