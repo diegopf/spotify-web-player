e1acdadaf98d9ce007bfcbd3d03db33280b75209
Diego Pascual <diegopascual.f@gmail.com>
2021-09-14 18:54:12 +0200

    test: add missing module to app test

--- apps/spotify-web-player/src/app/app.component.spec.ts
+++ apps/spotify-web-player/src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ThemeSwitchModule } from '@spotify-web-player/shared';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ThemeSwitchModule,
+      ],
     }).compileComponents();
   });
 
